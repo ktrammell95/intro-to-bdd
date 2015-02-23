70e5bdb41f1e3631ab3d9eeb9641eba4a53f2ca0
Katherine Trammell <ktrammell@gmail.com>
2015-02-23 17:10:14 -0500

    multiplies number to itself by number of times for exponent

--- test/test.bar.js
+++ test/test.bar.js
@@ -8,7 +8,7 @@ describe("Bar", function() {
   describe("squared", function() {
 
     it("takes a number and multiplies it by itself ", function() {
-      expect( typeof bar.squared ).eq("function");
+      expect( bar.squared(4) ).eq(16);
     });
 
   });
@@ -16,10 +16,24 @@ describe("Bar", function() {
   describe("cubed", function() {
 
       it("takes a number and multiplies it by itself 3 times ", function() {
-        expect( typeof bar.cubed ).eq("function");
+        expect( bar.cubed(2) ).eq(8);
       });
 
+  });
+
+  describe("exponentOf", function() {
+
+      it("multiplies number to itself by number of times for exponent", function() {
+      var exponentOf2 = bar.exponentOf(2);
+      expect( exponentOf2(4) ).eq(16);
     });
+
+  });
+
 
 
 });
+
+// var exponentOf2 = exponentOf(2);
+// exponentOf2(4);
+// 16
