48721e47f9b481e67965154475e4e57d58e8b475
Katherine Trammell <ktrammell@gmail.com>
2015-02-23 14:20:31 -0500

    returns a sorted copy of an array

--- js/foo.js
+++ js/foo.js
@@ -7,10 +7,13 @@ window.Foo = (function() {
   Foo.prototype = {
     plusOne: function(num) {
       return num += 1;
-    }
+    },
+    sortArray: function(array) {
+      return _.sortBy(array)
+    }, 
+
+
   };
-
-
   return Foo;
 
 })();
